eb716d3fb0ffb497450ca5b02d8a416066b81ff4
RDS_07 <57568305+RDS-007@users.noreply.github.com>
2022-08-21 17:20:09 +0530

    grus: Add cloning script

---
+++ clone.sh
@@ -0,0 +1,24 @@
+clear
+
+
+# DT
+rm -rf device/xiaomi
+rm -rf vendor/xiaomi
+rm -rf kernel/xiaomi
+
+git clone git@github.com:xiaomi-grus/device_xiaomi_grus.git device/xiaomi/grus
+git clone git@github.com:xiaomi-grus/device_xiaomi_sdm710-common.git device/xiaomi/sdm710-common
+git clone git@github.com:xiaomi-grus/vendor_xiaomi.git vendor/xiaomi
+git clone --depth 1 git@github.com:xiaomi-grus/kernel_xiaomi_grus.git kernel/xiaomi/sdm710
+
+
+# Hals
+rm -rf hardware/xiaomi
+
+git clone git@github.com:xiaomi-grus/android_hardware_xiaomi.git hardware/xiaomi
+
+
+# Clang
+rm -rf prebuilts/clang/host/linux-x86/clang-proton
+
+git clone --depth 1 https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-proton
